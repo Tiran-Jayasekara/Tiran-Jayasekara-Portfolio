b432432d1911f04b9d42d74978ce4008a28d0151
Tiran Jayasekara <70306643+Tiran-Jayasekara@users.noreply.github.com>
2026-03-28 18:38:30 +0530

    Removed
    Removed

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: '/Tiran-Jayasekara-Portfolio',
-  assetPrefix: '/Tiran-Jayasekara-Portfolio/',
 
   images: {
     unoptimized: true,
